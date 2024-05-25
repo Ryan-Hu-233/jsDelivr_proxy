b3d0f7310cd730e07f53260d3b84bdbe6ac69ef5
V <104217168+V-Official-233@users.noreply.github.com>
2024-05-25 18:06:31 +0800

    add file

---
+++ main.js
@@ -0,0 +1,32 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+// Create a proxy server with custom application logic
+var proxy = httpProxy.createProxyServer({ secure: false });
+
+// Modify Referer and Host header when proxing requests
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  proxyReq.setHeader('Referer', 'https://cdn.jsdelivr.net/');
+  proxyReq.setHeader('Host', 'cdn.jsdelivr.net');
+});
+
+// Catch errors
+proxy.on('error', function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Something went wrong. And we are reporting a custom error message.');
+  console.log(err, req, res);
+});
+
+var server = http.createServer(function(req, res) {
+  // Handle request with proxy
+  //console.log(req.rawHeaders);
+  proxy.web(req, res, {
+    target: 'https://cdn.jsdelivr.net'
+  });
+});
+
+const PORT = process.env.PORT || 3002;
+console.log("listening on port " + PORT.toString())
+server.listen(PORT);
